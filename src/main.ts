dac766aa90340dbdcc14fb4e10450711bfe492c8
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-18 00:06:15 -0300

    liberando portas"

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
+
+  // Habilita CORS para o frontend local
+  app.enableCors({
+    origin: ['*'], // ou '*' se quiser liberar para tudo (não recomendado em produção)
+    credentials: true,
+  });
   await app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });
